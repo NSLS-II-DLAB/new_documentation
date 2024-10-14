3caa3b1b9fe8b01a69dabedb045b3b651ddfa015
Darkkid819 <darkkid8192004@gmail.com>
2024-10-14 13:42:55 -0400

    fixed logging singals and refactored startup

--- src/megatron/logger.py
+++ src/megatron/logger.py
@@ -4,39 +4,30 @@ from datetime import datetime
 from bluesky.utils import make_decorator
 
 def ts_periodic_logging_wrapper(plan, signals, log_file_path, period=1):
+
     stop = asyncio.Event()
 
     async def logging_coro():
         while not stop.is_set():
             is_new_file = False
             if not os.path.isfile(log_file_path):
-                os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+                dir, _ = os.path.split(log_file_path)
+                os.makedirs(dir, exist_ok=True)
                 is_new_file = True
 
             timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
 
             with open(log_file_path, "at") as f:
                 if is_new_file:
-                    signal_names = ",".join([f"\"{_}\"" for _ in signals.keys()])
-                    f.write(f"Timestamp,{signal_names}\n")
-
-                signal_values = []
-                for signal in signals.values():
-                    if signal.connected:
-                        try:
-                            reading = await signal.read()
-                            signal_value = reading['value']
-                        except Exception as e:
-                            signal_value = f"Error: {str(e)}"
-                    else:
-                        signal_value = "Disconnected"
-                    signal_values.append(str(signal_value))
-
-                f.write(f"{timestamp},{','.join(signal_values)}\n")
+                    s = ",".join([f'"{_}"' for _ in signals.keys()])
+                    f.write(f"Timestamp,{s}\n")
+                s = ",".join([f"{_.value}" for _ in signals.values()])
+                f.write(f"{timestamp},{s}\n")
 
             await asyncio.sleep(period)
 
-    class StartStopLogging:
+    class StartStopLogging(object):
+
         def __enter__(self):
             print("Starting periodic logging")
             asyncio.ensure_future(logging_coro())
@@ -50,5 +41,6 @@ def ts_periodic_logging_wrapper(plan, signals, log_file_path, period=1):
             yield from plan
 
     return (yield from _inner())
+
 
 ts_periodic_logging_decorator = make_decorator(ts_periodic_logging_wrapper)
